4008748c033b3d735823792c12c3f807a7ed0de8
DESKTOP-4HV9UQO\user <yugi-0922@outlook.jp>
2023-05-01 14:30:46 +0900

    station7まで完了

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 // DO NOT DELETE
 
-import * as React from 'react'
-import './App.css'
-
+import * as React from 'react';
+import './App.css';
+import Header from './Header';
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1155.jpg',
+  );
+
+  // function changeImg() {
+  //   setDogUrl(
+  //     'https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3326.jpg',
+  //   );
+  //   get();
+  // }
+
+  async function changeImg() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (json) {
+        console.log(json);
+        console.log(json.message);
+        setDogUrl(json.message);
+        //var ps = JSON.parse(json.message);
+        //console.log(ps);
+        // for (const data of json) {
+        //   console.log(`${data.message}` + `${data.status}`);
+        // }
+      });
+  }
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
-  )
-}
+    <>
+      <Header></Header>
+      <div>
+        <p>犬の画像を表示するサイトです</p>
+      </div>
+      <div>
+        <img src={dogUrl} alt="" />
+      </div>
+      <button onClick={changeImg}>更新</button>
+    </>
+  );
+};
--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,20 @@
 // DO NOT DELETE
+
+import * as React from 'react';
+import './App.css';
+
+/**
+ *
+ * @type {React.FC}
+ */
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <h2>yugiアプリ</h2>
+      </header>
+    </>
+  );
+};
+
+export default Header;
